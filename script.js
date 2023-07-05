b2512fb64b7cac455b32d953e64c5de0ac00841d
gilmarperonjunior <109175794+DevJuniorP@users.noreply.github.com>
2023-07-05 08:37:17 -0300

    q
    q

---
+++ script.js
@@ -0,0 +1,11 @@
+const msg = console.log("Mandando Oi pro amigo")
+
+function mandaMensagem() {
+    console.log("Tudo bem");
+    console.log("Vou te mandar uma solicitção");
+    console.log("Solicitação recebida :)");
+}
+
+mandaMensagem();
+
+console.log("Tchau")
