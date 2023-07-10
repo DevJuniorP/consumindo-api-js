39a0d9884cc33fab4c1e91819b287d68453453e9
gilmarperonjunior <109175794+DevJuniorP@users.noreply.github.com>
2023-07-10 08:19:49 -0300

    promise all

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => console.log(r))
-
-console.log(consultaCEP);
+async function buscaEndereco(cep) {
+    try{
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPconvertida = await consultaCEP.json(); 
+        if (consultaCEPconvertida.erro) {
+            throw Error('CEP não existente');
+        }
+        console.log(consultaCEPconvertida);
+        return consultaCEPconvertida;
+    } catch(erro) {
+        console.log(erro)
+    }
+}
